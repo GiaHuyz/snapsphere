0c439fb1d57e7a0f245d1c844ebc8f1978064b3b
limbanga <95570833+limbanga@users.noreply.github.com>
2024-12-02 16:56:54 +0700

    add auth with clerk

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { log } from 'console'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ClerkExpressWithAuth } from '@clerk/clerk-sdk-node';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
@@ -10,10 +11,13 @@ async function bootstrap() {
   .setTitle('API Documentation')
   .setDescription('API documentation for the project')
   .setVersion('1.0')
+	.addBearerAuth()
   .build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
 	log('[DEV]',`Swagger is running on http://localhost:${process.env.PORT || 8000}/api`)
+	// config clerk
+  app.use(ClerkExpressWithAuth());
 	// start app
 	await app.listen(process.env.PORT ?? 8000)
 	log('[DEV]',`Server is running on http://localhost:${process.env.PORT || 8000}` )
