08b582c44334ccc2fd41365360725b218fcfd975
limbanga <95570833+limbanga@users.noreply.github.com>
2025-01-06 01:38:13 +0700

    feat: enhance CreateReportDto with Swagger API properties and improve code formatting

--- backend/src/reports/dto/create-report.dto.ts
+++ backend/src/reports/dto/create-report.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Transform } from 'class-transformer'
 import { IsEnum, IsMongoId, IsNotEmpty, IsOptional, IsString, ValidateIf } from 'class-validator'
 import mongoose, { isObjectIdOrHexString } from 'mongoose'
@@ -16,21 +17,34 @@ enum ReportReason {
 }
 
 enum ReportType {
-    PIN = 'pin',
-    COMMENT = 'comment',
-    USER = 'user'
+	PIN = 'pin',
+	COMMENT = 'comment',
+	USER = 'user'
 }
 
 export class CreateReportDto {
-	@IsString()
-    @IsNotEmpty()
-    item_id: string
 
+	@ApiProperty({
+		example: '60b9f7a7d1b0f0001f000001',
+		description: 'The id of the item that is being reported',
+	})
+	@IsString()
+	@IsNotEmpty()
+	item_id: string
+
+	@ApiProperty({
+		example: 'spam',
+		description: 'The reason for the report: "spam|nudity|self-harm|misinformation|hate|dangerous|harassment|violence|privacy|intellectual-property"',	
+	})
 	@IsNotEmpty()
 	@IsEnum(ReportReason)
 	reason: ReportReason
 
-    @IsNotEmpty()
-    @IsEnum(ReportType)
-    type: ReportType
+	@ApiProperty({
+		example: 'pin',
+		description: 'The type of the item that is being reported: "pin|comment|user"',
+	})
+	@IsNotEmpty()
+	@IsEnum(ReportType)
+	type: ReportType
 }
--- backend/src/reports/reports.controller.ts
+++ backend/src/reports/reports.controller.ts
@@ -11,30 +11,30 @@ import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
 @ApiBearerAuth()
 @Controller('reports')
 export class ReportsController {
-	constructor(private readonly reportsService: ReportsService) {}
+	constructor(private readonly reportsService: ReportsService) { }
 
-    @ApiOperation({ summary: 'Create report' })
+	@ApiOperation({ summary: 'Create report' })
 	@Post()
 	create(@Body() createReportDto: CreateReportDto, @UserId() userId: string) {
 		return this.reportsService.create(createReportDto, userId)
 	}
 
-    @ApiOperation({ summary: 'Get all reports only for admin' })
+	@ApiOperation({ summary: 'Get all reports only for admin' })
 	@UseGuards(AdminGuard)
 	@Get()
 	findAll(@Query() query: FilterReportDto) {
 		return this.reportsService.findAll(query)
 	}
 
-    @ApiOperation({ summary: 'Approve report only for admin' })
-    @UseGuards(AdminGuard)
+	@ApiOperation({ summary: 'Approve report only for admin' })
+	@UseGuards(AdminGuard)
 	@Patch(':id')
 	update(@Param('id') id: string, @Body() updateReportDto: UpdateReportDto) {
 		return this.reportsService.update(id, updateReportDto)
 	}
 
-    @ApiOperation({ summary: 'Delete report only for admin' })
-    @UseGuards(AdminGuard)
+	@ApiOperation({ summary: 'Delete report only for admin' })
+	@UseGuards(AdminGuard)
 	@Delete(':id')
 	remove(@Param('id') id: string) {
 		return this.reportsService.remove(id)
